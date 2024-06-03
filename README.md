2492a87bf281b4dee16abf7fb3d8e10e1f78885e
somaz <genius5711@gmail.com>
2024-06-03 15:42:11 +0900

    test: commit-info-extractor action template

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 # Container Action Template
 
-[![GitHub Super-Linter](https://github.com/actions/container-action/actions/workflows/linter.yml/badge.svg)](https://github.com/super-linter/super-linter)
+[![GitHub Super-Linter](
+https://github.com/actions/container-action/actions/workflows/linter.yml/badge.svg
+)](https://github.com/super-linter/super-linter)
 ![CI](https://github.com/actions/container-action/actions/workflows/ci.yml/badge.svg)
 
 ## License
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
 
 # Fetch commit messages with optional pretty formatting
-COMMIT_MESSAGES=$(git log -$INPUT_COMMIT_LIMIT ${INPUT_PRETTY:+--pretty=%B})
+COMMIT_MESSAGES=$(git log -"$INPUT_COMMIT_LIMIT" "${INPUT_PRETTY:+--pretty=%B}")
+if [ $? -ne 0 ]; then
+    echo "Error fetching commit messages"
+    exit 1
+fi
 
 echo "Commit Messages: $COMMIT_MESSAGES"
 
@@ -22,7 +26,7 @@ OUTPUT_VAR=${INPUT_OUTPUT_VARIABLE:-ENVIRONMENT}
 if [ -n "$GITHUB_ENV" ]; then
 	# GitHub Actions environment
 	echo "$OUTPUT_VAR=$ENVIRONMENT" >> "$GITHUB_ENV"
-	echo "$OUTPUT_VAR=$ENVIRONMENT" >> "$GITHUB_OUTPUT"
+	echo "::set-output name=$OUTPUT_VAR::$ENVIRONMENT"
 else
 	# Local execution
 	echo "Final Environment Variable ($OUTPUT_VAR): $ENVIRONMENT"
