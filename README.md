d03c3fcbda68c62d452cc74e83941c095a6eead6
somaz <genius5711@gmail.com>
2024-06-18 14:16:18 +0900

    fix: ci.yml & entrypoint.sh

--- README.md
+++ README.md
@@ -2,7 +2,8 @@
 
 [![GitHub Super-Linter](
 https://github.com/actions/container-action/actions/workflows/linter.yml/badge.svg
-)](https://github.com/super-linter/super-linter)
+)]
+(https://github.com/super-linter/super-linter)
 ![CI](https://github.com/actions/container-action/actions/workflows/ci.yml/badge.svg)
 
 ## License
--- entrypoint.sh
+++ entrypoint.sh
@@ -2,21 +2,21 @@
 
 # Fetch commit messages with optional pretty formatting
 if ! COMMIT_MESSAGES=$(git log -"$INPUT_COMMIT_LIMIT" "${INPUT_PRETTY:+--pretty=%B}"); then
-  echo "Error fetching commit messages"
-  exit 1
+	echo "Error fetching commit messages"
+	exit 1
 fi
 
 echo "Commit Messages: $COMMIT_MESSAGES"
 
 # Use the provided command to extract information.
 if [ -n "$INPUT_EXTRACT_COMMAND" ]; then
-    # Using eval to execute the command, ensuring the command is wrapped in quotes for proper handling
-    if ! ENVIRONMENT=$(echo "$COMMIT_MESSAGES" | eval "$INPUT_EXTRACT_COMMAND"); then
-        echo "Error extracting environment information"
-        exit 1
-    fi
+	# Using eval to execute the command, ensuring the command is wrapped in quotes for proper handling
+	if ! ENVIRONMENT=$(echo "$COMMIT_MESSAGES" | eval "$INPUT_EXTRACT_COMMAND"); then
+		echo "Error extracting environment information"
+		exit 1
+	fi
 else
-    ENVIRONMENT="$COMMIT_MESSAGES"
+	ENVIRONMENT="$COMMIT_MESSAGES"
 fi
 
 echo "Extracted Environment: $ENVIRONMENT"
@@ -26,10 +26,10 @@ OUTPUT_VAR=${INPUT_OUTPUT_VARIABLE:-ENVIRONMENT}
 
 # Conditional handling for GitHub Actions or local execution
 if [ -n "$GITHUB_ENV" ]; then
-    # GitHub Actions environment
-    echo "$OUTPUT_VAR=$ENVIRONMENT" >> "$GITHUB_ENV"
-    echo "::set-output name=$OUTPUT_VAR::$ENVIRONMENT"
+	# GitHub Actions environment
+	echo "$OUTPUT_VAR=$ENVIRONMENT" >>"$GITHUB_ENV"
+	echo "::set-output name=$OUTPUT_VAR::$ENVIRONMENT"
 else
-    # Local execution
-    echo "Final Environment Variable ($OUTPUT_VAR): $ENVIRONMENT"
+	# Local execution
+	echo "Final Environment Variable ($OUTPUT_VAR): $ENVIRONMENT"
 fi
