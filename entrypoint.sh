1c983c274ffa217dec2930cbafee66c8323fe537
somaz <genius5711@gmail.com>
2024-06-18 12:49:08 +0900

    fix: entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,20 +1,22 @@
 #!/bin/sh
 
 # Fetch commit messages with optional pretty formatting
-COMMIT_MESSAGES=$(git log -"$INPUT_COMMIT_LIMIT" "${INPUT_PRETTY:+--pretty=%B}")
-if [ $? -ne 0 ]; then
-    echo "Error fetching commit messages"
-    exit 1
+if ! COMMIT_MESSAGES=$(git log -"$INPUT_COMMIT_LIMIT" "${INPUT_PRETTY:+--pretty=%B}"); then
+  echo "Error fetching commit messages"
+  exit 1
 fi
 
 echo "Commit Messages: $COMMIT_MESSAGES"
 
 # Use the provided command to extract information.
 if [ -n "$INPUT_EXTRACT_COMMAND" ]; then
-	# Using eval to execute the command, ensuring the command is wrapped in quotes for proper handling
-	ENVIRONMENT=$(echo "$COMMIT_MESSAGES" | eval "$INPUT_EXTRACT_COMMAND")
+    # Using eval to execute the command, ensuring the command is wrapped in quotes for proper handling
+    if ! ENVIRONMENT=$(echo "$COMMIT_MESSAGES" | eval "$INPUT_EXTRACT_COMMAND"); then
+        echo "Error extracting environment information"
+        exit 1
+    fi
 else
-	ENVIRONMENT="$COMMIT_MESSAGES"
+    ENVIRONMENT="$COMMIT_MESSAGES"
 fi
 
 echo "Extracted Environment: $ENVIRONMENT"
@@ -24,10 +26,10 @@ OUTPUT_VAR=${INPUT_OUTPUT_VARIABLE:-ENVIRONMENT}
 
 # Conditional handling for GitHub Actions or local execution
 if [ -n "$GITHUB_ENV" ]; then
-	# GitHub Actions environment
-	echo "$OUTPUT_VAR=$ENVIRONMENT" >> "$GITHUB_ENV"
-	echo "::set-output name=$OUTPUT_VAR::$ENVIRONMENT"
+    # GitHub Actions environment
+    echo "$OUTPUT_VAR=$ENVIRONMENT" >> "$GITHUB_ENV"
+    echo "::set-output name=$OUTPUT_VAR::$ENVIRONMENT"
 else
-	# Local execution
-	echo "Final Environment Variable ($OUTPUT_VAR): $ENVIRONMENT"
+    # Local execution
+    echo "Final Environment Variable ($OUTPUT_VAR): $ENVIRONMENT"
 fi
